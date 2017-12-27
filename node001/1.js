3ee8164948412b98f08b34e4c4935748c2db9dc5
huangjx <trustfor@sina.cn>
2017-12-27 10:48:41 +0800

    我的第一个服务器

---
+++ node001/1.js
@@ -0,0 +1,10 @@
+//命令行打开当前目录  node 1.js就可以运行node node里没有 BOM 和 DOM
+console.log(1);
+
+var oDate=new Date();
+
+console.log(oDate.getFullYear());
+
+var re=/\d+/g;
+
+console.log("sdf343 34534ree r5345345g 345".match(re));
